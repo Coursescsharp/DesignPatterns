93a1e8e38eddbaed74770ba234a83f620a62ea6f
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2023-08-08 15:32:47 +0200

    Point example

---
+++ DesignPatterns/DesignPatterns/Enums/CoordinateSystem.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace DesignPatterns.Enums;
+
+public enum CoordinateSystem
+{
+    [EnumMember(Value = "Cartesian")]
+    Cartesian = 1,
+
+    [EnumMember(Value = "Polar")]
+    Polar = 2
+}
---
+++ DesignPatterns/DesignPatterns/Models/Point.cs
@@ -0,0 +1,35 @@
+﻿using DesignPatterns.Enums;
+
+namespace DesignPatterns.Models;
+
+public class Point
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    /// <summary>
+    /// Initializes a point from either cartesian or polar.
+    /// </summary>
+    /// <param name="a">x if cartesian, rho if polar</param>
+    /// <param name="b">y if cartesian, theta if polar</param>
+    /// <param name="system">Coordiante system to use</param>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public Point(double a,
+                 double b,
+                 CoordinateSystem system = CoordinateSystem.Cartesian)
+    {
+        switch (system)
+        {
+            case CoordinateSystem.Cartesian:
+                X = a;
+                Y = b;
+                break;
+            case CoordinateSystem.Polar:
+                X = a * Math.Cos(b);
+                Y = a * Math.Sin(b);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(system), system, null);
+        }
+    }
+}
